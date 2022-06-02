60fea1ed28d073cbc21e628ee90e6f6bec63aca6
vmkuhn <104328744+vmkuhn@users.noreply.github.com>
2022-06-02 08:59:32 +0200

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://d01adf03.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
